729e1cc226b7f308bb035b58370e05fc9a7b975b
ChristhianSM <christhian2524@gmail.com>
2022-02-03 19:03:49 -0500

    Se agrego __dirname

---
+++ src/server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const { Contenedor } = require('../models/UserManagerProducts')
+
+const app = express();
+
+//Settings
+app.set('port', 4000)
+const contenedor = new Contenedor(path.join(__dirname + '/../files/products.txt'));
+
+//Routes
+app.get('/', (req, res) => {
+    res.send({mensaje: 'Hola Mundo'})
+})
+
+app.get('/productos', async (req, res) => {
+    const productos = await contenedor.getAll();
+    res.send(productos.payload)
+})
+
+app.get('/productoRandom', async (req, res) => {
+    const { payload } = await contenedor.getAll();
+
+    const quantityProducts = payload.length;
+    const random = Math.floor((Math.random() * quantityProducts));
+    res.send(payload[random])
+})
+
+//Listenning the server
+const server = app.listen(app.get('port'), () => {
+    console.log(`Servidor http escuchando en el puesto ${server.address().port}`);
+})
+
+server.on("error", error => console.log(`Error en servidor ${error}`))
